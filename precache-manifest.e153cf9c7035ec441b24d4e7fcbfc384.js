7efd0f376653195463f7e1c278a6e5cdff82982a
Arjun <arjungupta2110@gmail.com>
2020-02-10 22:03:38 +0000

    Updates

---
+++ precache-manifest.e153cf9c7035ec441b24d4e7fcbfc384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e384a821eda8323449115c41157ea09",
+    "url": "/hindi-flashcards/index.html"
+  },
+  {
+    "revision": "6a3ddb9e5b9edeee0dba",
+    "url": "/hindi-flashcards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "997a7afe9f0d9ca067d1",
+    "url": "/hindi-flashcards/static/js/2.a63dc360.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hindi-flashcards/static/js/2.a63dc360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3ddb9e5b9edeee0dba",
+    "url": "/hindi-flashcards/static/js/main.bba45587.chunk.js"
+  },
+  {
+    "revision": "1380c2959d057849740e",
+    "url": "/hindi-flashcards/static/js/runtime-main.bff2ef76.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d1fd03bf086089e5939021b8faa09e3.js"
+  "/hindi-flashcards/precache-manifest.e153cf9c7035ec441b24d4e7fcbfc384.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hindi-flashcards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
