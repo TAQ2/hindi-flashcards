8e8b5837e39e144a24e554b27502dabf0fe3d9d2
Arjun <arjungupta2110@gmail.com>
2020-02-10 21:58:41 +0000

    Updates

---
+++ precache-manifest.1d1fd03bf086089e5939021b8faa09e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adc8c1f62ab4ed331c3d045853808dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c062bb733ae8024ea80",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "997a7afe9f0d9ca067d1",
+    "url": "/static/js/2.a63dc360.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a63dc360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c062bb733ae8024ea80",
+    "url": "/static/js/main.76db4212.chunk.js"
+  },
+  {
+    "revision": "4b5b05a41f559a7d30db",
+    "url": "/static/js/runtime-main.b982344d.js"
+  }
+]);
