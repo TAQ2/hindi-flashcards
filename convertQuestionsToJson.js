c7949b23f53c9b44d24d6fbc25b391e035c60559
Arjun <arjungupta2110@gmail.com>
2020-02-12 16:55:01 +0000

    add more questions

--- convertQuestionsToJson.js
+++ convertQuestionsToJson.js
@@ -19,27 +19,3 @@ csv()
       }
     );
   });
-//   .on("json", jsonObj => {
-//     // combine csv header row and csv line to a json object
-//     // jsonObj.a ==> 1 or 4
-//     console.log(jsonObj);
-//   })
-//   .on("done", error => {
-//     console.log("end");
-//   });
-
-// converter.fromFile(file, function(err, result) {
-//   // if an error has occured then handle it
-
-//   console.log("json");
-
-//   if (err) {
-//     console.error("An Error Has Occured");
-//     console.error(err);
-//   }
-
-//   const json = JSON.stringify(result);
-
-//   console.log(json);
-
-// });
